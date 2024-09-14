81972b2123d980c156fa9d4e7e269833e873828a
murtaz <murtazasbazaz@gmail.com>
2024-09-14 13:28:46 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import '../styles/globals.css'
 import Navbar from '@/components/Navbar'
 import { ThemeProvider } from 'next-themes'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/react"
+
 export default function RootLayout({
   children,
 }: {
@@ -20,6 +22,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
         </ThemeProvider>
         </body>
     </html>
